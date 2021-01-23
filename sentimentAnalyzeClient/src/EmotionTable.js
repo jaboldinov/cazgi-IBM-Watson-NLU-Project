eb1655bff9f8aaf4a2c220d738d0a670830aaa4d
jaboldinov <jaboldinov@gmail.com>
2021-01-23 17:27:42 +0000

    added emotion table

--- sentimentAnalyzeClient/src/EmotionTable.js
+++ sentimentAnalyzeClient/src/EmotionTable.js
@@ -5,18 +5,16 @@ class EmotionTable extends React.Component {
     render() {
       return (  
         <div>
-          {/*You can remove this line and the line below. */}
-          {JSON.stringify(this.props.emotions)}
           <table className="table table-bordered">
             <tbody>
-            {
-                //Write code to use the .map method that you worked on in the Hands-on React lab to extract the emotions
+            {this.props.map((i, j) => 
+                (<tr><td>{i}</td><td>{j}</td></tr>)
+                )
             }
             </tbody>
           </table>
           </div>
           );
         }
-    
 }
 export default EmotionTable;
