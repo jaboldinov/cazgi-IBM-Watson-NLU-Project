edd9f59de82cd2bf7ddb494526a112929968d411
jaboldinov <jaboldinov@gmail.com>
2021-01-23 15:01:39 +0000

    added endpoints to sentimentAnalyzeServer.js

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+dotenv.config();
 
-app.use(express.static('client'))
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
 
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url
+    });
+
+    return naturalLanguageUnderstanding;
+}
+
+app.use(express.static('client'));
 const cors_app = require('cors');
 app.use(cors_app());
 
@@ -11,23 +30,73 @@ app.get("/",(req,res)=>{
   });
 
 app.get("/url/emotion", (req,res) => {
-
-    return res.send({"happy":"90","sad":"10"});
+    let ibmParams = {
+        url: req.query.url,
+        features: {
+            emotion: {
+                document: true,
+            }
+        }
+    };
+    getNLUInstance().analyze(ibmParams)
+    .then(resSend => {
+        return res.send(resSend) })
+    .catch(err => {
+        return res.send(err.toString());
+    });
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let ibmParams = {
+        url: req.query.url,
+        features: {
+            sentiment: {
+                document: true,
+            }
+        }
+    };
+    getNLUInstance().analyze(ibmParams)
+    .then(resSend => {
+        return res.send(resSend) })
+    .catch(err => {
+        return res.send(err.toString());
+    });
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+    let ibmParams = {
+        url: req.query.text,
+        features: {
+            emotion: {
+                document: true,
+            }
+        }
+    };
+    getNLUInstance().analyze(ibmParams)
+    .then(resSend => {
+        return res.send(resSend) })
+    .catch(err => {
+        return res.send(err.toString());
+    });
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let ibmParams = {
+        url: req.query.text,
+        features: {
+            sentiment: {
+                document: true,
+            }
+        }
+    };
+    getNLUInstance().analyze(ibmParams)
+    .then(resSend => {
+        return res.send(resSend) })
+    .catch(err => {
+        return res.send(err.toString());
+    });
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
-})
-
+});
